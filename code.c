a91d98a20a6b9f1b577a05a883a3cfee6dc6d80f
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-12-19 19:28:59 +0300

    Update code.c

--- code.c
+++ code.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
-
-#define N1 4
-#define N2 4
-
-void get_arr(int *arr, int len)
+#define N1 3
+#define N2 2
+void get_arr(int arr[][N2])
 {
-    for (int i = 0; i < len; i++)
-        *(arr + i) = i;
-}
-
-void print_arr(int *arr, int len)
-{
-    for (int i = 0; i < len; i++)
-        printf("%d ", *(arr + i));
-    putchar('\n');
-}
-
-void print_arr_second(int *arr, int len1, int len2, int n)
-{
-    for (int i = 0; i < len1; i++)
-    {
-        for (int j = 0; j < len2; j++)
+    puts("Введите значения элементов массива:");
+    for (int i=0;i!=N1;i++)
+        for (int j=0;j!=N2;j++)
         {
-            printf("%d ", *((arr + i * len2) + j));
-            if (i * len2 + j >= n)
-                return;
+            printf("arr[%d][%d]=",i,j);
+            scanf("%d", &arr[i][j]);
         }
-        putchar('\n');
-    }
 }
-
+ 
+void print_arr(int (*arr)[N2])
+{
+    puts("Элементы массива получили значения:");
+    for (int i=0; i<N1; i++)
+        for (int j=0; j<N2; j++)
+        {
+            printf("arr[%d][%d]=%d ", i, j, arr[i][j]);
+            if (j==N2-1)puts("");
+        }
+}
+ 
+void print_arr_second(int * arr, int len1, int len2, int n)
+{
+    int p=0,c=0;
+    for (int i=0; i!=len1; i++)
+        for (int j=0; j!=len2; j++)
+        {
+            c++;
+            printf("arr[%d][%d]=%d ",i,j,*(arr+p));
+            if (c==n){
+                c=0;
+                puts("");
+            }
+            p++;
+        }
+}
+ 
 int main()
 {
-    int array[N1][N2];
-    get_arr((int *)array, N1 * N2);
-    print_arr((int *)array, N1 * N2);
-    putchar('\n');
-    print_arr_second((int *)array, N1, N2, 8);
+    int a[N1][N2],n;
+    get_arr(a);
+    print_arr(a);
+    puts("Количество элементов, выводимых в строке таблицы:");
+    scanf("%d",&n);
+    print_arr_second(&a[0][0],N1,N2,n);
 }
